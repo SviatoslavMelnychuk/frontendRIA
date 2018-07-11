98b67e2e54002c71de5bc0bff2ce3c0b93370914
SviatoslavMelnychuk <melnychuksviatoslav@gmail.com>
2018-07-12 00:05:25 +0300

    show books

--- static/index.js
+++ static/index.js
@@ -1,1 +1,11 @@
-console.log("hi")
+let req = new XMLHttpRequest();
+req.open('GET',
+    'static/data.json', false);
+req.send(null);
+if(req.status == 200) {
+        let books = JSON.parse(req.responseText);
+        for( id in books){
+            let item =
+            console.log(books[id]);
+        }
+}
